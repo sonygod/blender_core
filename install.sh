2976b4ddd28f80a34581216a98eceb6c74337fcc
Mr zhan <sonygodx@gmail.com>
2023-10-06 06:48:41 +0000

    build ok

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 apt update
 sudo apt install build-essential git subversion cmake libx11-dev libxxf86vm-dev libxcursor-dev libxi-dev libxrandr-dev libxinerama-dev libegl-dev -y
 sudo apt install libwayland-dev wayland-protocols libxkbcommon-dev libdbus-1-dev linux-libc-dev -y
-cd ../../lib
+cd ../lib
 svn checkout https://svn.blender.org/svnroot/bf-blender/trunk/lib/linux_x86_64_glibc_228
-cd ../../blender
+cd ../blender_core
 
